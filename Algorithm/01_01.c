d7bc3c0409d84a26460d01292094647189202001
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2022-09-30 20:26:16 +0900

    Create 01_01.c

---
+++ Algorithm/01_01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int N,M;
+int arr[10000001] = {};
+int num;
+int main()
+{
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+  
+		{
+        scanf("%d", &num);
+        arr[num] = 1;
+    }
+    
+    scanf("%d", &M);
+    for (int i = 0; i < M; i++)
+    
+		{
+        scanf("%d", &num);
+        printf("%d ", arr[num]);
+    }
+    
+    return 0;
+}
