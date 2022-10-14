9ec079a4efabfbdb248824b2ba15240028f7224e
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2022-10-14 13:46:55 +0900

    Create 03_01.c

---
+++ Algorithm/03_01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int Arr[101][101] = {};
+    int n;
+    scanf("%d", &n);
+    int x = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        if(i%2 == 1){
+            for(int j = 1; j<=n; j++)
+            {
+                Arr[i][j] = x;
+                x++;
+            }
+        }
+        else{
+            for(int j = n; j>=1; j--)
+            {
+                Arr[i][j] = x;
+                x++;
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n ; j++)
+        {
+            printf("%d ",Arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
