9e3bc2ac6dd274b60dccd4b550464cd0395a3954
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-03-30 20:08:15 +0900

    Create 1546.c

---
+++ codeup/c/함수/1546.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int n;
+#include<stdbool.h>
+bool zero(int k)
+{
+    return !k;
+}
+bool plus(int k)
+{
+    return k>0;
+}
+int main()
+{
+  scanf("%d", &n);
+  if(zero(n)) printf("zero");
+  else printf("%s", plus(n)?"plus":"minus");
+  return 0;
+}
