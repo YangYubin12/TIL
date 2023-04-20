6eb4bbb4d655dd95f7966c02c5f9e7774402102b
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-04-21 08:18:45 +0900

    Create 1547.c

---
+++ codeup/c/함수/1547.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int n;
+#include<stdbool.h>
+bool prime(int k)
+{
+    int i;
+    for(i=2;i<k;i++)
+        if(k%i==0)
+            break;
+    return i==k;
+}
+int main()
+{
+  scanf("%d", &n);
+  if(prime(n)) printf("prime");
+  else printf("composite");
+  return 0;
+}
