29a171f0657e02329601f9a29592e42f8ff82aeb
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-06-09 23:01:29 +0900

    Create 1474.c

---
+++ codeup/c/2차원배열/1474.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(void)
+{
+    int Arr[101][101] = {};
+    int n,m;
+    scanf("%d %d", &n, &m);
+    int x = 1;
+    int k = 1;
+    for(int i = m; i >= 1; i--){
+        if(k%2 == 0){
+            for(int j = 1; j<=n; j++){
+                Arr[j][i] = x;
+                x++;
+            }
+        }
+        else{
+            for(int j = n; j>=1; j--){
+                Arr[j][i] = x;
+                x++;
+            }
+        }
+        k++;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m ; j++){
+            printf("%d ",Arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
