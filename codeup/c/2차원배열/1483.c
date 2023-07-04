4f77c0e177616eacc15a2434a6b89e2d9b9268a5
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-07-04 22:08:37 +0900

    Create 1483.c

---
+++ codeup/c/2차원배열/1483.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(void)
+{
+    int Arr[101][101] = {};
+    int n,m;
+    scanf("%d %d", &n, &m);
+    int x = 1;
+    int i = n;
+    int j = 1;
+    int a = i;
+    int b = j;
+    for(int k = 1; k <=n*m; k++){
+        if( ( i >= 1 && i <= n) && ( j >= 1 && j <= m) ){
+            Arr[i][j] = x;
+            x++;
+            i++;
+            j++;
+            if( !(( i >= 1 && i <= n) && ( j >= 1 && j <= m)) ){ 
+                a--; 
+                if(a <= 0){ 
+                    a = 1;
+                    b++;
+                }
+                i = a;
+                j = b;
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m ; j++){
+            printf("%d ",Arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
