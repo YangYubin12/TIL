d5d6defe79c893b2d13386eb404c9f022e2e1ae2
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-08-11 23:55:42 +0900

    Create 1548.c

---
+++ codeup/c/함수/1548.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int n;
+
+char grade(int n)
+{
+    if (n >= 90)
+    {
+        return 'A';
+    }
+    else if (n >= 80)
+    {
+        return 'B';
+    }
+    else if (n >= 70)
+    {
+        return 'C';
+    }
+    else if (n >= 60)
+    {
+        return 'D';
+    }
+    return 'F';
+}
+
+int main()
+{
+  scanf("%d", &n);
+  printf("%c", grade(n));
+  return 0;
+}
