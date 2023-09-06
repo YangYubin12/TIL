50aa5fe9f2cb4e7882ebdeb199dbd68cd218f2e3
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-09-06 23:52:59 +0900

    Create 1571.c

---
+++ codeup/c/함수/1571.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int n, k, d[1010];
+int upper_bound(k)
+{
+	for (int i = 1; i <= n; i++)
+		if (d[i] > k)
+			return i;
+	return n + 1;
+}
+
+int main()
+{
+   scanf("%d", &n);
+
+  for(int i=1; i<=n; i++)
+    scanf("%d", &d[i]);
+
+  scanf("%d", &k);
+
+  printf("%d\n", upper_bound(k));
+}
